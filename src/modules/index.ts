e66c2446920e07d25a153e20a37162e8d1cb0a24
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2024-04-13 16:34:01 +0500

    add: UserModule to 'modules collector'

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,7 @@
 import { Module, Type } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 
-const modules: Type<any>[] = [];
+const modules: Type<any>[] = [UserModule];
 
 @Module({
   imports: [...modules],
