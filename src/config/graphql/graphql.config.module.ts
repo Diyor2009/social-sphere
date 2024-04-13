0f64b79fb39d9f56ef2203802678e1479c678098
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2024-04-13 16:32:16 +0500

    bugfix: use ObjectIdScalar in schema

--- src/config/graphql/graphql.config.module.ts
+++ src/config/graphql/graphql.config.module.ts
@@ -2,7 +2,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import GraphQLJSON from 'graphql-type-json';
+import { ObjectIdScalar } from './scalars/objectId.scalar';
 
 @Module({
   imports: [
@@ -14,9 +14,9 @@ import GraphQLJSON from 'graphql-type-json';
         path: '/',
         playground: true,
         autoSchemaFile: true,
-        resolvers: { JSON: GraphQLJSON },
       }),
     }),
   ],
+  providers: [ObjectIdScalar],
 })
 export class GraphQLConfigModule {}
